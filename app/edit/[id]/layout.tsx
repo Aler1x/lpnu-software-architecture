54ecb97a66db9955b255b77e80f9c93b2c739d92
Aler1x <95098796+Aler1x@users.noreply.github.com>
2024-12-04 21:43:50 +0200

    fix: build, make api/ open

--- app/edit/[id]/layout.tsx
+++ app/edit/[id]/layout.tsx
@@ -1,18 +1,12 @@
 
 import Header from '@/components/header';
-import DiagramPage from './page';
 import { cn } from '@/lib/utils';
 
-export default async function RootLayout({
-  params,
-}: {
-  params: Promise<{ id: string }>
-}) {
-  const id = (await params).id
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className={cn('min-h-dvh flex flex-col')}>
       <Header />
-      <DiagramPage id={id} />
+      {children}
     </div>
   );
 }
